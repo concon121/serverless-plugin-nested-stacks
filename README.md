cfb018ddc63a90d44c45efc76988ca3e7c1ce871
Connor Bray <connor.bray@icloud.com>
2017-11-12 18:51:13 +0000

    Inisital commit of nested templates resource

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # serverless-plugin-nested-stacks
-Nested stacks for sls
+
+Nested stacks for the Serverless Framework!
+
+## Configuration Reference
+
+```
+custom:
+  nested-stacks:
+    location: nested-stacks                      # Where do you keep your nested stacks?
+    stacks:
+      - id: MyGroovyNestedStack                  # Logical ID (Required)
+        template: nested-template.yml            # Template file name (Required)
+        timeout: 60                              # Minutes before stack creation times out.
+        parameters:                              # Stack parameters as key value pairs
+          - InstanceType: t1.micro
+          - BlahBlah: abc123
+        tags:                                    # Stack tags
+          - ${file(nested-stacks/core-tags.yml)} # Load tags from a file?
+          - CustomTag: Yolo
+        notifications:                           # Notification ARN's for SNS
+          - arn:aws:sns:region:account-id:topicname
+
+```
+* **custom.nested-stacks.location** - (Required) Organisation is important.  Keep all your nested stacks in one place and use this attribute to define where that place is.
+* **custom.nested-stacks.stacks** - (Required) Your very own list of nested stack definitions!
+* **custom.nested-stacks.stacks.id** - (Required) The logical ID of the nested stack resource.
+* **custom.nested-stacks.stacks.template** - (Required) The file name of the nested stack.  Remember, this file must exist in the directory defined by _custom.nested-stacks.location_.
+* **custom.nested-stacks.stacks.timeout** - Time in minutes before the stack creation times out.
+* **custom.nested-stacks.stacks.parameters** - A list of key value pairs to be passed into the nested stack as parameters.
+* **custom.nested-stacks.stacks.tags** - A list of key value pairs to be passed into the nested stack as its tags.
+* **custom.nested-stacks.stacks.notifications** - A list of existing Amazon SNS topics where notifications about stack events are sent.
+
+## Want to know more?
+
+Read the AWS Cloudformation documentation for [AWS::Cloudformation::Stack](http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-stack.html) resources!
---
+++ index.js
@@ -0,0 +1,140 @@
+'use strict'
+
+const BbPromise = require('bluebird')
+const fs = require('fs')
+const path = require('path')
+const setBucketName = require('serverless/lib/plugins/aws/lib/setBucketName')
+const ref = {}
+
+class AWSNestedStacks {
+    constructor(serverless, options) {
+        const self = this
+        ref.self = self
+        this.serverless = serverless
+        this.service = serverless.service
+        this.serverlessLog = serverless.cli.log.bind(serverless.cli)
+        this.options = options
+        this.provider = this.serverless.getProvider('aws')
+        Object.assign(self, setBucketName)
+
+        this.hooks = {
+            'before:package:finalize': self.createNestedStackCfn,
+            'before:aws:deploy:deploy:updateStack': () => BbPromise.bind(this)
+        .then(self.setBucketName)
+        .then(self.uploadNestedTemplates)
+      // 'before:deploy:*': self.start(),
+      // 'before:config:*': self.start()
+        }
+    }
+
+    getBaseUrl() {
+        var baseUrl
+        const service = ref.self.serverless.service.service
+        const stage = ref.self.options.stage
+        if (ref.self.provider.deploymentBucket) {
+            baseUrl = 'https://s3.amazonaws.com/' + ref.self.provider.deploymentBucket + `/serverless/${service}/${stage}`
+        } else {
+            baseUrl = {
+                'Fn::Sub': 'https://s3.amazonaws.com/${ServerlessDeploymentBucket}' + `/serverless/${service}/${stage}`
+            }
+        }
+        return baseUrl
+    }
+
+    getTemplateUrl(baseUrl, stack) {
+        var templateUrl
+        if (typeof baseUrl === 'object') {
+            templateUrl = baseUrl
+            templateUrl['Fn::Sub'] += `/${stack.template}`
+        } else {
+            templateUrl = baseUrl + `/${stack.template}`
+        }
+        return templateUrl
+    }
+
+    mergeArray(array) {
+        let tagObj = {}
+        if (Array.isArray(array)) {
+            for (let item of array) {
+                Object.assign(tagObj, item)
+            }
+        } else {
+            tagObj = array
+        }
+        return tagObj
+    }
+
+    generateTags(stack) {
+        let tags = []
+        stack.tags = ref.self.mergeArray(stack.tags)
+        for (let property in stack.tags) {
+            if (stack.tags.hasOwnProperty(property)) {
+                let newTag = {}
+                newTag.Key = property
+                newTag.Value = stack.tags[property]
+                tags.push(newTag)
+            }
+        }
+        return tags
+    }
+
+    createNestedStackCfn() {
+        ref.self.serverlessLog('Creating nested stacks in Cloudformation...')
+        let stacks = ref.self.serverless.service.custom['nested-stacks'].stacks
+        if (stacks) {
+            const baseUrl = ref.self.getBaseUrl()
+            let resources = ref.self.serverless.service.provider.compiledCloudFormationTemplate.Resources
+            for (let stack of stacks) {
+                if (stack && stack.id && stack.template) {
+                    ref.self.serverlessLog('Stack: ' + stack.template)
+                    resources[stack.id] = {
+                        'Type': 'AWS::CloudFormation::Stack',
+                        'Properties': {
+                            'TemplateURL': ref.self.getTemplateUrl(baseUrl, stack)
+                        }
+                    }
+                    if (stack.notifications) {
+                        resources[stack.id].Properties.NotificationARNs = stack.notifications
+                    }
+                    if (stack.parameters) {
+                        let params = ref.self.mergeArray(stack.parameters)
+                        resources[stack.id].Properties.Parameters = params
+                    }
+                    if (stack.tags) {
+                        let tags = ref.self.generateTags(stack)
+                        resources[stack.id].Properties.Tags = tags
+                    }
+                    if (stack.timeout) {
+                        resources[stack.id].Properties.TimeoutInMinutes = stack.timeout
+                    }
+                } else {
+                    var msg = 'Missing required properties for nested stack:\n'
+                    msg += '\tid - Logical ID of the nested stack\n'
+                    msg += '\ttemplate - the name of the nested cloudformation templates'
+                    throw new Error(msg)
+                }
+            }
+        }
+    }
+
+    uploadNestedTemplates() {
+        ref.self.serverlessLog('Uploading nested stacks to S3...')
+        const service = ref.self.serverless.service.service
+        const stage = ref.self.options.stage
+        const self = this
+        let templateFolder = ref.self.serverless.service.custom['nested-stacks'].location || '.'
+        let stacks = ref.self.serverless.service.custom['nested-stacks'].stacks
+        for (let stack of stacks) {
+            ref.self.serverlessLog('Stack: ' + stack.template)
+            let body = fs.readFileSync(path.join(templateFolder, stack.template), 'utf8')
+            let params = {
+                Bucket: self.bucketName,
+                Key: `serverless/${service}/${stage}/${stack.template}`,
+                Body: body,
+                ContentType: 'application/json'
+            }
+            ref.self.provider.request('S3', 'putObject', params, ref.self.options.stage, ref.self.options.region)
+        }
+    }
+}
+module.exports = AWSNestedStacks
